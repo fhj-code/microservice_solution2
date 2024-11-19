88b5cbbc5834d4e9799bc121ae9db537d46ec87b
Host <qr.deb4@gmail.com>
2024-11-19 18:46:34 +1000

    File tree added

---
+++ microservice_solution2/AppInterfaces/IActivityLogger.cs
@@ -0,0 +1,10 @@
+﻿using microservice_solution2.Models;
+using System.Collections.Generic;
+
+namespace microservice_solution2
+{
+    public interface IActivityLogger
+    {
+        IEnumerable<LogEvent> Get(long timestamp);
+    }
+}
---
+++ microservice_solution2/AppInterfaces/Models/CartEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace microservice_solution2.Models
+{
+    public class CartEvent: EventBase
+    {
+        public CartEventTypeEnum Type { get; set; }
+        public Order Order { get; set; }
+    }
+}
